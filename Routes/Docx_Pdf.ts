ad8576b344366677dc86ffdbc9057cccc571f0ad
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-11-29 19:01:00 +0530

    docx uploaded by the user can now be converted to pdf and the user can download it.

--- Routes/Docx_Pdf.ts
+++ Routes/Docx_Pdf.ts
@@ -1,4 +1,6 @@
 import express, {Express, Router, Request, Response} from "express";
+import {convert} from "libreoffice-convert";
+import * as fs from "fs";
 
 const router: Router = express.Router();
 
@@ -16,8 +18,38 @@ router.get("/", async(req: Request, res: Response) => {
 });
 
 router.post("/", upload.single("UploadedFile"), async(req: Request, res: Response) => {
-    console.log(req.file);
-    return res.status(200).json({message: "File Uploaded Successfully."});
+    if(req.file) {
+        console.log(req.file);
+        if(req.file.path.slice(-4) === "docx") {
+            const inputPath: string = req.file.path;
+            const outputPath: string = inputPath + ".pdf";
+            const toConvert: Buffer = fs.readFileSync(inputPath);
+            convert(toConvert, ".pdf", undefined, async(err, result) => {
+                if(err) {
+                    console.log(err);
+                    fs.unlinkSync(inputPath);
+                    return res.status(500).json({message: "Internal server error."});
+                }
+                fs.writeFileSync(outputPath, result);
+                return res.status(200).download(outputPath, (err2) => {
+                    if(err2) {
+                        console.log(err2);
+                        fs.unlinkSync(inputPath);
+                        fs.unlinkSync(outputPath);
+                        return res.status(500).json({messgae: "Internal server error."});
+                    }
+                    fs.unlinkSync(inputPath);
+                    fs.unlinkSync(outputPath);
+                });
+            });
+        }
+        else {
+            return res.status(400).json({message: "Selected file is not DOCX"});
+        }
+    }
+    else {
+        return res.status(400).json({message: "Please select a file"});
+    }
 });
 
 module.exports = router;
